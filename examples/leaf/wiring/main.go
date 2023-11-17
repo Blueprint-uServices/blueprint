5a72f1a621a08fc6f05a386ab43251bf47cbbd97
Jonathan Mace <jonathan.c.mace@gmail.com>
2023-11-16 17:05:19 -0800

    Update the leaf example to use the cmdbuilder

--- examples/leaf/wiring/main.go
+++ examples/leaf/wiring/main.go
@@ -1,81 +1,25 @@
+// Package main provides the LeafApp application, primarily used for
+// testing compilation during the plugin development process.
+//
+// Run with go run examples/leaf/wiring/main.go
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"gitlab.mpi-sws.org/cld/blueprint/blueprint/pkg/wiring"
-	"gitlab.mpi-sws.org/cld/blueprint/plugins/clientpool"
-	"gitlab.mpi-sws.org/cld/blueprint/plugins/dockerdeployment"
-	"gitlab.mpi-sws.org/cld/blueprint/plugins/goproc"
-	"gitlab.mpi-sws.org/cld/blueprint/plugins/grpc"
-	"gitlab.mpi-sws.org/cld/blueprint/plugins/healthchecker"
-	"gitlab.mpi-sws.org/cld/blueprint/plugins/linuxcontainer"
-	"gitlab.mpi-sws.org/cld/blueprint/plugins/simplecache"
-	"gitlab.mpi-sws.org/cld/blueprint/plugins/simplenosqldb"
+	"gitlab.mpi-sws.org/cld/blueprint/examples/leaf/wiring/specs"
+	"gitlab.mpi-sws.org/cld/blueprint/plugins/wiringcmd"
 	"gitlab.mpi-sws.org/cld/blueprint/plugins/workflow"
-	"gitlab.mpi-sws.org/cld/blueprint/plugins/workload"
-	"golang.org/x/exp/slog"
 )
 
-func serviceDefaults(spec wiring.WiringSpec, serviceName string) string {
-	procName := fmt.Sprintf("proc%s", serviceName)
-	ctrName := fmt.Sprintf("ctr%s", serviceName)
-	// opentelemetry.Instrument(spec, serviceName)
-	clientpool.Create(spec, serviceName, 5)
-	healthchecker.AddHealthCheckAPI(spec, serviceName)
-	grpc.Deploy(spec, serviceName)
-	goproc.CreateProcess(spec, procName, serviceName)
-	return linuxcontainer.CreateContainer(spec, ctrName, procName)
-}
-
 func main() {
-
-	fmt.Println("Constructing spec Spec")
-
-	// Initialize blueprint compiler
-	linuxcontainer.RegisterAsDefaultBuilder()
-	dockerdeployment.RegisterAsDefaultBuilder()
-
-	// --------------
-
-	spec := wiring.NewWiringSpec("leaf_example")
-
-	// Create the wiring spec
+	// Configure the location of our workflow spec
 	workflow.Init("../workflow")
 
-	b_database := simplenosqldb.Define(spec, "b_database")
-	b_cache := simplecache.Define(spec, "b_cache")
-	b := workflow.Define(spec, "b", "LeafServiceImpl", b_cache, b_database)
-
-	a := workflow.Define(spec, "a", "NonLeafService", b)
-
-	pa := serviceDefaults(spec, a)
-	pb := serviceDefaults(spec, b)
-
-	client := workload.Generator(spec, a)
-
-	// -----------------
-
-	slog.Info("spec Spec: \n" + spec.String())
-
-	// Build the IR for our specific nodes
-	nodesToInstantiate := []string{pa, pb, client}
-	application, err := spec.BuildIR(nodesToInstantiate...)
-	if err != nil {
-		slog.Error("Unable to build blueprint, exiting", "error", err)
-		slog.Info("Application: \n" + application.String())
-		os.Exit(1)
-	}
-
-	slog.Info("Application: \n" + application.String())
-
-	// Below here is a WIP on generating code
-
-	err = application.GenerateArtifacts("tmp")
-	if err != nil {
-		slog.Error(err.Error())
-		os.Exit(1)
-	}
-	fmt.Println("Exiting")
+	// Build a supported wiring spec
+	name := "LeafApp"
+	wiringcmd.MakeAndExecute(
+		name,
+		specs.Docker,
+		specs.Thrift,
+		specs.HTTP,
+	)
 }
---
+++ examples/leaf/wiring/specs/thrift.go
@@ -0,0 +1,42 @@
+package specs
+
+import (
+	"fmt"
+
+	"gitlab.mpi-sws.org/cld/blueprint/blueprint/pkg/wiring"
+	"gitlab.mpi-sws.org/cld/blueprint/plugins/clientpool"
+	"gitlab.mpi-sws.org/cld/blueprint/plugins/goproc"
+	"gitlab.mpi-sws.org/cld/blueprint/plugins/mongodb"
+	"gitlab.mpi-sws.org/cld/blueprint/plugins/simplecache"
+	"gitlab.mpi-sws.org/cld/blueprint/plugins/thrift"
+	"gitlab.mpi-sws.org/cld/blueprint/plugins/wiringcmd"
+	"gitlab.mpi-sws.org/cld/blueprint/plugins/workflow"
+	"gitlab.mpi-sws.org/cld/blueprint/plugins/workload"
+)
+
+var Thrift = wiringcmd.SpecOption{
+	Name:        "thrift",
+	Description: "Deploys each service in a separate process, communicating using Thrift.",
+	Build:       makeThriftSpec,
+}
+
+func makeThriftSpec(spec wiring.WiringSpec) ([]string, error) {
+	leaf_db := mongodb.PrebuiltContainer(spec, "leaf_db")
+	leaf_cache := simplecache.Define(spec, "leaf_cache")
+	leaf_service := workflow.Define(spec, "leaf_service", "LeafServiceImpl", leaf_cache, leaf_db)
+	leaf_proc := applyThriftDefaults(spec, leaf_service)
+
+	nonleaf_service := workflow.Define(spec, "nonleaf_service", "NonLeafService", leaf_service)
+	nonleaf_proc := applyThriftDefaults(spec, nonleaf_service)
+
+	app_client := workload.Generator(spec, nonleaf_service)
+
+	return []string{leaf_proc, nonleaf_proc, app_client}, nil
+}
+
+func applyThriftDefaults(spec wiring.WiringSpec, serviceName string) string {
+	procName := fmt.Sprintf("%s_process", serviceName)
+	clientpool.Create(spec, serviceName, 5)
+	thrift.Deploy(spec, serviceName)
+	return goproc.CreateProcess(spec, procName, serviceName)
+}
