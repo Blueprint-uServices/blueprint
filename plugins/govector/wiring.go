344db06636935ffa5c464399f87834d4e07e1488
Vaastav Anand <vaastav.anand05@gmail.com>
2023-11-18 03:31:08 +0100

    Add initial empty files for govector

---
+++ plugins/govector/wiring.go
@@ -0,0 +1,8 @@
+package govector
+
+import "gitlab.mpi-sws.org/cld/blueprint/blueprint/pkg/wiring"
+
+// Instruments the service with an entry + exit point govector wrapper to generate govector logs.
+func Instrument(spec wiring.WiringSpec, serviceName string) {
+
+}
