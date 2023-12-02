a5bb5efe578cd23c190a6adb1c1f684dcc65e7eb
Vaastav Anand <vaastav.anand05@gmail.com>
2023-12-02 13:34:39 +0100

    Re-organize test datA

--- examples/dsb_sn/tests/usermentionservice_test.go
+++ examples/dsb_sn/tests/usermentionservice_test.go
@@ -6,56 +6,13 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"gitlab.mpi-sws.org/cld/blueprint/examples/DSB_sn/workflow/socialnetwork"
-	"gitlab.mpi-sws.org/cld/blueprint/runtime/core/backend"
 	"gitlab.mpi-sws.org/cld/blueprint/runtime/core/registry"
-	"gitlab.mpi-sws.org/cld/blueprint/runtime/plugins/simplecache"
-	"gitlab.mpi-sws.org/cld/blueprint/runtime/plugins/simplenosqldb"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 var userMentionServiceRegistry = registry.NewServiceRegistry[socialnetwork.UserMentionService]("userMention_service")
 
-var userCacheRegistry = registry.NewServiceRegistry[backend.Cache]("user_cache")
-
-var userDBRegistry = registry.NewServiceRegistry[backend.NoSQLDatabase]("user_db")
-
-var vaastav = socialnetwork.User{
-	FirstName: "Vaastav",
-	LastName:  "Anand",
-	UserID:    5,
-	Username:  "vaastav",
-}
-
-var jcmace = socialnetwork.User{
-	FirstName: "Jonathan",
-	LastName:  "Mace",
-	UserID:    2,
-	Username:  "jcmace",
-}
-
-var antoinek = socialnetwork.User{
-	FirstName: "Antoine",
-	LastName:  "Kaufmann",
-	UserID:    1,
-	Username:  "antoinek",
-}
-
-var dg = socialnetwork.User{
-	FirstName: "Deepak",
-	LastName:  "Garg",
-	UserID:    3,
-	Username:  "dg",
-}
-
 func init() {
-
-	userCacheRegistry.Register("local", func(ctx context.Context) (backend.Cache, error) {
-		return simplecache.NewSimpleCache(ctx)
-	})
-
-	userDBRegistry.Register("local", func(ctx context.Context) (backend.NoSQLDatabase, error) {
-		return simplenosqldb.NewSimpleNoSQLDB(ctx)
-	})
 
 	userMentionServiceRegistry.Register("local", func(ctx context.Context) (socialnetwork.UserMentionService, error) {
 		cache, err := userCacheRegistry.Get(ctx)
--- examples/dsb_sn/tests/userservice_test.go
+++ examples/dsb_sn/tests/userservice_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"gitlab.mpi-sws.org/cld/blueprint/examples/DSB_sn/workflow/socialnetwork"
+	"gitlab.mpi-sws.org/cld/blueprint/runtime/core/backend"
 	"gitlab.mpi-sws.org/cld/blueprint/runtime/core/registry"
 	"gitlab.mpi-sws.org/cld/blueprint/runtime/plugins/simplecache"
 	"gitlab.mpi-sws.org/cld/blueprint/runtime/plugins/simplenosqldb"
@@ -11,7 +12,48 @@ import (
 
 var userServiceRegistry = registry.NewServiceRegistry[socialnetwork.UserService]("user_service")
 
+var userCacheRegistry = registry.NewServiceRegistry[backend.Cache]("user_cache")
+
+var userDBRegistry = registry.NewServiceRegistry[backend.NoSQLDatabase]("user_db")
+
+var vaastav = socialnetwork.User{
+	FirstName: "Vaastav",
+	LastName:  "Anand",
+	UserID:    5,
+	Username:  "vaastav",
+}
+
+var jcmace = socialnetwork.User{
+	FirstName: "Jonathan",
+	LastName:  "Mace",
+	UserID:    2,
+	Username:  "jcmace",
+}
+
+var antoinek = socialnetwork.User{
+	FirstName: "Antoine",
+	LastName:  "Kaufmann",
+	UserID:    1,
+	Username:  "antoinek",
+}
+
+var dg = socialnetwork.User{
+	FirstName: "Deepak",
+	LastName:  "Garg",
+	UserID:    3,
+	Username:  "dg",
+}
+
 func init() {
+
+	userCacheRegistry.Register("local", func(ctx context.Context) (backend.Cache, error) {
+		return simplecache.NewSimpleCache(ctx)
+	})
+
+	userDBRegistry.Register("local", func(ctx context.Context) (backend.NoSQLDatabase, error) {
+		return simplenosqldb.NewSimpleNoSQLDB(ctx)
+	})
+
 	userServiceRegistry.Register("local", func(ctx context.Context) (socialnetwork.UserService, error) {
 		cache, err := simplecache.NewSimpleCache(ctx)
 		if err != nil {
