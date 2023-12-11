67241747165ef2a5652bcc4d8d5d2b692fd9732c
Vaastav Anand <vaastav.anand05@gmail.com>
2023-12-11 15:13:23 +0100

    Add documentation for circuitbreaker compiler plugin

--- plugins/circuitbreaker/wiring.go
+++ plugins/circuitbreaker/wiring.go
@@ -1,3 +1,6 @@
+// Package circuitbreaker provides a Blueprint modifier for the client side of service calls.
+//
+// The plugin wraps clients with a circuitbreaker that blocks any new requests from being sent out over a connection if the failure rate exceeds a provided number in a fixed duration. The block is removed after the completion of the fixed duration interval.
 package circuitbreaker
 
 import (
