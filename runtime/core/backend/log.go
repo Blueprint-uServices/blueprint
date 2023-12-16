48e0825e30f3a6af5bdcf6a826a512a710c3617b
Vaastav Anand <vaastav.anand05@gmail.com>
2023-12-16 03:08:58 +0100

    Add basic log api

---
+++ runtime/core/backend/log.go
@@ -0,0 +1,71 @@
+package backend
+
+import (
+	"context"
+
+	"golang.org/x/exp/slog"
+)
+
+// The Priority Level at which the message will be recorded
+type Priority int
+
+const (
+	TRACE Priority = iota
+	DEBUG
+	INFO
+	WARN
+	ERROR
+)
+
+// Represents a Key-Value pair associated with a particular log msg
+type Attribute struct {
+	Key   string
+	Value interface{}
+}
+
+// Represents a logger that can be used by the logger plugin
+type Logger interface {
+	// Log creates a new log record at the desired `priority` level with `msg` as the log message and `attr` as list of optional key-value pairs for logging messages.
+	// Returns a context that may-be updated by the logger with some logger specific state. If no state is set, then the passed-in context is returned as is.
+	Log(ctx context.Context, priority Priority, msg string, attr ...Attribute) context.Context
+}
+
+var logger Logger
+
+// Blueprint's default logger that uses the slog package
+type defaultLogger struct{}
+
+func (l *defaultLogger) Log(ctx context.Context, priority Priority, msg string, attrs ...Attribute) context.Context {
+	var args []interface{}
+	for _, attr := range attrs {
+		args = append(args, attr.Key)
+		args = append(args, attr.Value)
+	}
+	switch priority {
+	case TRACE:
+	case DEBUG:
+		slog.Debug(msg, args)
+		break
+	case INFO:
+		slog.Info(msg, args)
+		break
+	case WARN:
+		slog.Info(msg, args)
+		break
+	case ERROR:
+		slog.Error(msg, args)
+	}
+	return ctx
+}
+
+// Set's the default logger to be used by the Blueprint application.
+// NOTE: This should not be called in the workflow code. This is called from the various logger plugins.
+func SetDefaultLogger(l Logger) {
+	logger = l
+}
+
+// Log function exposed to Blueprint workflow applications.
+// Wraps around the currently set default logger's Logger API calls.
+func Log(ctx context.Context, priority Priority, msg string, attrs ...Attribute) context.Context {
+	return logger.Log(ctx, priority, msg, attrs...)
+}
