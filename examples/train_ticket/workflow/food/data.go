fcea55a7db9ef504ce0092a937535bebc1fd372c
Vaastav Anand <vaastav.anand05@gmail.com>
2023-12-20 08:55:49 +0100

    Add Food Order struct

--- examples/train_ticket/workflow/food/data.go
+++ examples/train_ticket/workflow/food/data.go
@@ -4,3 +4,13 @@ type Food struct {
 	Name  string
 	Price float64
 }
+
+type FoodOrder struct {
+	ID          string
+	OrderID     string
+	FoodType    int64
+	StationName string
+	StoreName   string
+	FoodName    string
+	Price       float64
+}
