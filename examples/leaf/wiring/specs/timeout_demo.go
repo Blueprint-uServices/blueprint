0146a6a62303dd9947bcd370ffd8df0f3bf8a29c
Vaastav Anand <vaastav.anand05@gmail.com>
2023-12-21 03:54:12 +0100

    Fix-up timeout usage in the leaf application

--- examples/leaf/wiring/specs/timeout_demo.go
+++ examples/leaf/wiring/specs/timeout_demo.go
@@ -10,8 +10,8 @@ import (
 	"gitlab.mpi-sws.org/cld/blueprint/plugins/latencyinjector"
 	"gitlab.mpi-sws.org/cld/blueprint/plugins/linuxcontainer"
 	"gitlab.mpi-sws.org/cld/blueprint/plugins/mongodb"
-	"gitlab.mpi-sws.org/cld/blueprint/plugins/retries"
 	"gitlab.mpi-sws.org/cld/blueprint/plugins/simple"
+	"gitlab.mpi-sws.org/cld/blueprint/plugins/timeouts"
 	"gitlab.mpi-sws.org/cld/blueprint/plugins/wiringcmd"
 	"gitlab.mpi-sws.org/cld/blueprint/plugins/workflow"
 )
@@ -37,10 +37,9 @@ func makeDockerTimeoutSpec(spec wiring.WiringSpec) ([]string, error) {
 func applyDockerTimeoutDefaults(spec wiring.WiringSpec, serviceName string) string {
 	procName := fmt.Sprintf("%s_process", serviceName)
 	ctrName := fmt.Sprintf("%s_container", serviceName)
-	// opentelemetry.Instrument(spec, serviceName)
-	//timeouts.AddTimeouts(spec, serviceName, "100ms")
+	timeouts.AddTimeouts(spec, serviceName, "100ms")
 	// Uncomment this to try out retries with timeouts functionality
-	retries.AddRetriesWithTimeouts(spec, serviceName, 10, "100ms")
+	//retries.AddRetriesWithTimeouts(spec, serviceName, 10, "100ms")
 	latencyinjector.AddFixedLatency(spec, serviceName, "200ms")
 	healthchecker.AddHealthCheckAPI(spec, serviceName)
 	http.Deploy(spec, serviceName)
