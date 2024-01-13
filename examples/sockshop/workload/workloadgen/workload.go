32c06d5202eddb4ca5c2ada8f8dd3a4d17ffc544
Jonathan Mace <jonathan.c.mace@gmail.com>
2024-01-12 16:09:36 -0800

    simple workloadgen func

--- examples/sockshop/workload/workloadgen/workload.go
+++ examples/sockshop/workload/workloadgen/workload.go
@@ -1,5 +1,13 @@
 package workloadgen
 
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/blueprint-uservices/blueprint/examples/sockshop/workflow/frontend"
+)
+
 // The WorkloadGen interface, which the Blueprint compiler will treat as a
 // Workflow service
 type WorkloadGen interface {
@@ -9,4 +17,22 @@ type WorkloadGen interface {
 // workloadGen implementation
 type workloadGen struct {
 	WorkloadGen
+
+	frontend frontend.Frontend
+}
+
+func NewWorkloadGen(ctx context.Context, frontend frontend.Frontend) (WorkloadGen, error) {
+	return &workloadGen{frontend: frontend}, nil
+}
+
+func (s *workloadGen) Run(ctx context.Context) error {
+	ticker := time.NewTicker(1 * time.Second)
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case t := <-ticker.C:
+			fmt.Println("Tick at", t)
+		}
+	}
 }
