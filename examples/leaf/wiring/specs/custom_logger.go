b4aaf2f9bf4cd413d46c796fd961bb218a8192bf
Vaastav Anand <vaastav.anand05@gmail.com>
2024-01-17 17:03:24 +0100

    Update documentation for openetelemetry, jaeger, and zipkin

--- examples/leaf/wiring/specs/custom_logger.go
+++ examples/leaf/wiring/specs/custom_logger.go
@@ -7,10 +7,12 @@ import (
 	"github.com/blueprint-uservices/blueprint/plugins/goproc"
 	"github.com/blueprint-uservices/blueprint/plugins/http"
 	"github.com/blueprint-uservices/blueprint/plugins/linuxcontainer"
+	"github.com/blueprint-uservices/blueprint/plugins/opentelemetry"
 	"github.com/blueprint-uservices/blueprint/plugins/simple"
 	"github.com/blueprint-uservices/blueprint/plugins/wiringcmd"
 	"github.com/blueprint-uservices/blueprint/plugins/workflow"
 	"github.com/blueprint-uservices/blueprint/plugins/xtrace"
+	"github.com/blueprint-uservices/blueprint/plugins/zipkin"
 )
 
 // Wiring specification demonstrates how to use a custom logger for Blueprint applications.
@@ -23,22 +25,44 @@ var Xtrace_Logger = wiringcmd.SpecOption{
 	Build:       makeXTraceLoggerSpec,
 }
 
+// Wiring specification demonstrates how to use a custom logger for Blueprint applications.
+// The wiring spec uses opentelemetry logger as the custom logger for demonstration.
+// Each service is deployed in a separate container with services communicating using HTTP.
+// Launches an zipkin server which collects spans generated from each service wrapped in opentelemetry tracing.
+var OT_Logger = wiringcmd.SpecOption{
+	Name:        "ot_logger",
+	Description: "Deploys each service in a separate container, communicating using HTTP. Wraps each service in opentelemetry tracing and sets the OTLogger for each process. All spans are collected by the zipkin collector.",
+	Build:       makeOTLoggerSpec,
+}
+
+func makeOTLoggerSpec(spec wiring.WiringSpec) ([]string, error) {
+	return makeCustomLoggerSpec(spec, "ot")
+}
+
 func makeXTraceLoggerSpec(spec wiring.WiringSpec) ([]string, error) {
 	return makeCustomLoggerSpec(spec, "xtrace")
 }
 
 func makeCustomLoggerSpec(spec wiring.WiringSpec, logger_type string) ([]string, error) {
+	var collector string
+	if logger_type == "ot" {
+		collector = zipkin.Collector(spec, "zipkin")
+	}
 	applyLoggerDefaults := func(service_name string) string {
 
 		procName := strings.ReplaceAll(service_name, "service", "process")
 		if logger_type == "xtrace" {
 			xtrace.Instrument(spec, service_name)
+		} else if logger_type == "ot" {
+			opentelemetry.Instrument(spec, service_name, collector)
 		}
 		cntrName := strings.ReplaceAll(service_name, "service", "container")
 		http.Deploy(spec, service_name)
 		goproc.CreateProcess(spec, procName, service_name)
 		if logger_type == "xtrace" {
 			xtrace.Logger(spec, procName)
+		} else if logger_type == "ot" {
+			opentelemetry.Logger(spec, service_name)
 		}
 		return linuxcontainer.CreateContainer(spec, cntrName, procName)
 	}
