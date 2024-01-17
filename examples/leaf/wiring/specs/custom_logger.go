9206a92c2c5cb35cae4621ed9dc922f3f5318a39
Vaastav Anand <vaastav.anand05@gmail.com>
2024-01-17 10:51:31 +0100

    [Logging] Add Xtrace logger implementation (#146)
    * Adds xtrace logger implementation and a corresponding demonstrative specification using xtrace logger for the leaf application.
    
    * Moves xtrace interface to runtime/plugin folder from backend folder
    
    * Updates the documentation for the xtrace plugin package

---
+++ examples/leaf/wiring/specs/custom_logger.go
@@ -0,0 +1,54 @@
+package specs
+
+import (
+	"strings"
+
+	"github.com/blueprint-uservices/blueprint/blueprint/pkg/wiring"
+	"github.com/blueprint-uservices/blueprint/plugins/goproc"
+	"github.com/blueprint-uservices/blueprint/plugins/http"
+	"github.com/blueprint-uservices/blueprint/plugins/linuxcontainer"
+	"github.com/blueprint-uservices/blueprint/plugins/simple"
+	"github.com/blueprint-uservices/blueprint/plugins/wiringcmd"
+	"github.com/blueprint-uservices/blueprint/plugins/workflow"
+	"github.com/blueprint-uservices/blueprint/plugins/xtrace"
+)
+
+// Wiring specification demonstrates how to use a custom logger for Blueprint applications.
+// The wiring spec uses xtrace logger as the custom logger for demonstration.
+// Each service is deployed in a separate container with services communicating using HTTP.
+// Launches an XTrace server with each service wrapped in xtrace tracing.
+var Xtrace_Logger = wiringcmd.SpecOption{
+	Name:        "xtrace_logger",
+	Description: "Deploys each service in a separate container, communicating using HTTP. Wraps each service in XTrace tracing and sets the XTraceLogger for each process.",
+	Build:       makeXTraceLoggerSpec,
+}
+
+func makeXTraceLoggerSpec(spec wiring.WiringSpec) ([]string, error) {
+	return makeCustomLoggerSpec(spec, "xtrace")
+}
+
+func makeCustomLoggerSpec(spec wiring.WiringSpec, logger_type string) ([]string, error) {
+	applyLoggerDefaults := func(service_name string) string {
+
+		procName := strings.ReplaceAll(service_name, "service", "process")
+		if logger_type == "xtrace" {
+			xtrace.Instrument(spec, service_name)
+		}
+		cntrName := strings.ReplaceAll(service_name, "service", "container")
+		http.Deploy(spec, service_name)
+		goproc.CreateProcess(spec, procName, service_name)
+		if logger_type == "xtrace" {
+			xtrace.Logger(spec, procName)
+		}
+		return linuxcontainer.CreateContainer(spec, cntrName, procName)
+	}
+	leaf_db := simple.NoSQLDB(spec, "leaf_db")
+	leaf_cache := simple.Cache(spec, "leaf_cache")
+	leaf_service := workflow.Service(spec, "leaf_service", "LeafServiceImpl", leaf_cache, leaf_db)
+	leaf_proc := applyLoggerDefaults(leaf_service)
+
+	nonleaf_service := workflow.Service(spec, "nonleaf_service", "NonLeafService", leaf_service)
+	nonleaf_proc := applyLoggerDefaults(nonleaf_service)
+
+	return []string{leaf_proc, nonleaf_proc}, nil
+}
---
+++ plugins/xtrace/ir_xtrace_logger.go
@@ -0,0 +1,86 @@
+package xtrace
+
+import (
+	"fmt"
+
+	"github.com/blueprint-uservices/blueprint/blueprint/pkg/coreplugins/address"
+	"github.com/blueprint-uservices/blueprint/blueprint/pkg/coreplugins/service"
+	"github.com/blueprint-uservices/blueprint/blueprint/pkg/ir"
+	"github.com/blueprint-uservices/blueprint/plugins/golang"
+	"github.com/blueprint-uservices/blueprint/plugins/golang/gocode"
+	"github.com/blueprint-uservices/blueprint/plugins/golang/goparser"
+	"github.com/blueprint-uservices/blueprint/plugins/workflow"
+	"golang.org/x/exp/slog"
+)
+
+// Blueprint IR Node that represents a process-level xtrace logger
+type XTraceLogger struct {
+	golang.Node
+	golang.Instantiable
+
+	ServerDialAddr *address.DialConfig
+
+	LoggerName  string
+	Iface       *goparser.ParsedInterface
+	Constructor *gocode.Constructor
+}
+
+func newXTraceLogger(name string, addr *address.DialConfig) (*XTraceLogger, error) {
+	node := &XTraceLogger{}
+	err := node.init(name)
+	if err != nil {
+		return nil, err
+	}
+	node.LoggerName = name
+	node.ServerDialAddr = addr
+	return node, nil
+}
+
+func (node *XTraceLogger) init(name string) error {
+	workflow.Init("../../runtime")
+	spec, err := workflow.GetSpec()
+	if err != nil {
+		return err
+	}
+
+	details, err := spec.Get("XTraceLogger")
+	if err != nil {
+		return err
+	}
+
+	node.Iface = details.Iface
+	node.Constructor = details.Constructor.AsConstructor()
+	return nil
+}
+
+func (node *XTraceLogger) Name() string {
+	return node.LoggerName
+}
+
+func (node *XTraceLogger) String() string {
+	return node.Name() + " = XTraceLogger(" + node.ServerDialAddr.Name() + ")"
+}
+
+func (node *XTraceLogger) AddToWorkspace(builder golang.WorkspaceBuilder) error {
+	return golang.AddRuntimeModule(builder)
+}
+
+func (node *XTraceLogger) AddInterfaces(builder golang.ModuleBuilder) error {
+	return node.AddToWorkspace(builder.Workspace())
+}
+
+func (node *XTraceLogger) GetInterface(ctx ir.BuildContext) (service.ServiceInterface, error) {
+	return node.Iface.ServiceInterface(ctx), nil
+}
+
+func (node *XTraceLogger) AddInstantiation(builder golang.NamespaceBuilder) error {
+	if builder.Visited(node.LoggerName) {
+		return nil
+	}
+
+	slog.Info(fmt.Sprintf("Instantiating XTraceLogger %v in %v/%v", node.LoggerName, builder.Info().Package.PackageName, builder.Info().FileName))
+
+	return builder.DeclareConstructor(node.LoggerName, node.Constructor, []ir.IRNode{node.ServerDialAddr})
+}
+
+func (node *XTraceLogger) ImplementsGolangNode() {}
