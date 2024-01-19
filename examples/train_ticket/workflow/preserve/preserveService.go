b9f3a672cb3a909466a3429c62e10f842d1b61aa
Vaastav Anand <vaastav.anand05@gmail.com>
2024-01-19 10:00:20 +0100

    WIP services

---
+++ examples/train_ticket/workflow/preserve/preserveService.go
@@ -0,0 +1,2 @@
+// Package preserve implements ts-preserve-service from the original Train Ticket application
+package preserve
---
+++ examples/train_ticket/workflow/travelplan/travelPlanService.go
@@ -0,0 +1,5 @@
+// Package travelplan implements ts-travel-plan service from the original Train Ticket application
+package travelplan
+
+type TravelPlanService interface {
+}
