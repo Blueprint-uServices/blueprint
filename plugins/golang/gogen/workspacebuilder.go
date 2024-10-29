5902a9377ed2cdb544a3d4751b42a9aff8c209d6
Vaastav Anand <vaastav.anand05@gmail.com>
2024-10-29 19:03:32 +0100

    [Bugfix ]Update go work version issue
    Fixes #179

--- plugins/golang/gogen/workspacebuilder.go
+++ plugins/golang/gogen/workspacebuilder.go
@@ -167,7 +167,7 @@ func (workspace *WorkspaceBuilderImpl) readModfile(moduleSubDir string) (*modfil
 	return f, nil
 }
 
-var goWorkTemplate = `go 1.22
+var goWorkTemplate = `go 1.22.1
 
 use (
 	{{ range $dirName, $moduleName := .Modules }}./{{ $dirName }}
