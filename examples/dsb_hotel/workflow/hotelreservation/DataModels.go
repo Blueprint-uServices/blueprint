2b63a6f2eb29211259e46ef9d4b38d3d65b7b1f2
Vaastav Anand <vaastav.anand05@gmail.com>
2026-01-25 23:34:13 +0100

    Start adding Attractons Servce'

--- examples/dsb_hotel/workflow/hotelreservation/DataModels.go
+++ examples/dsb_hotel/workflow/hotelreservation/DataModels.go
@@ -97,6 +97,8 @@ type Image struct {
 	Default bool
 }
 
+func (i Image) remote() {}
+
 type Review struct {
 	ReviewId    string
 	HotelId     string
@@ -105,3 +107,45 @@ type Review struct {
 	Description string
 	Img         Image
 }
+
+func (r Review) remote() {}
+
+type Restaurant struct {
+	RestaurantId   string  `bson:"restaurantId"`
+	RLat           float64 `bson:"lat"`
+	RLon           float64 `bson:"lon"`
+	RestaurantName string  `bson:"restaurantName"`
+	Rating         float32 `bson:"rating"`
+	Type           string  `bson:"type"`
+}
+
+func (r Restaurant) remote()       {}
+func (r *Restaurant) Lat() float64 { return r.RLat }
+func (r *Restaurant) Lon() float64 { return r.RLon }
+func (r *Restaurant) Id() string   { return r.RestaurantId }
+
+type Museum struct {
+	MuseumId   string  `bson:"museumId"`
+	MLat       float64 `bson:"lat"`
+	MLon       float64 `bson:"lon"`
+	MuseumName string  `bson:"museumName"`
+	Type       string  `bson:"type"`
+}
+
+func (m Museum) remote()       {}
+func (m *Museum) Lat() float64 { return m.MLat }
+func (m *Museum) Lon() float64 { return m.MLon }
+func (m *Museum) Id() string   { return m.MuseumId }
+
+type Cinema struct {
+	CinemaId   string  `bson:"cinemaId"`
+	CLat       float64 `bson:"lat"`
+	CLon       float64 `bson:"lon"`
+	CinemaName string  `bson:"cinemaName"`
+	Type       string  `bson:"type"`
+}
+
+func (c Cinema) remote()       {}
+func (c *Cinema) Lat() float64 { return c.CLat }
+func (c *Cinema) Lon() float64 { return c.CLon }
+func (c *Cinema) Id() string   { return c.CinemaId }
